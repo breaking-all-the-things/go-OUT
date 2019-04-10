fc35076c238386e1408098d2dac9f7a66632c2e3
Alex <alex@Alex.local>
2019-04-10 19:48:34 -0400

    ajax calls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,4 +13,19 @@
 
  var currentConditionsUrl = "http://dataservice.accuweather.com/currentconditions/v1"+ location + "A9IYPehiyBlSicaf0AMQF9lZsMQMnLnH";
  var forecastConditionsUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + location + "A9IYPehiyBlSicaf0AMQF9lZsMQMnLnH";
- var eventbrite = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + location + "&location.within=10mi&token=K4DUF7V3GIFZDMZYFH57"
+//  var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?location.address=" + location + "&location.within=10mi&token=K4DUF7V3GIFZDMZYFH57";
+
+ $("#search-btn").on("click", function (locationSearch) {
+  var eventbriteURL = "https://www.eventbriteapi.com/v3/events/search/?q=" + location + "&location.within=10mi&token=K4DUF7V3GIFZDMZYFH57";
+
+var location = $("#location-input").val().trim();
+
+$.ajax({
+  url: eventbriteURL,
+  query: "GET",
+}).then(function(response) {
+console.log(response);
+
+})
+console.log(location); 
+ });
